b2120828d1474ce7d9c04d0c32bd375e48551127
Rizky Ertanto <45276983+ertantorizkyf@users.noreply.github.com>
2020-02-11 14:46:03 +0700

    Update twitter_api.php

--- twitter_api.php
+++ twitter_api.php
@@ -3,10 +3,10 @@
 
     use Abraham\TwitterOAuth\TwitterOAuth;
 
-    $consumer_key = 'QdBanYqxM8RzWOpJolFIdVW5V';
-    $consumer_secret = '5bzJB6U8hkHnvcx3nzHc1SPUNpL7wbC5rG8qz447GsdAhMjev7';
-    $access_token = '1216944596262895616-l33xxrdsCkxjJO84XS0pMCZ26YPTZZ';
-    $access_token_secret = 'kPYBioyeQN4h20qvguUisNpAgDbFCDuiWSfVij87eiif0';
+    $consumer_key = 'CONSUMER_API_KEY';
+    $consumer_secret = 'CONSUMER_API_SECRET';
+    $access_token = 'ACCESS_TOKEN';
+    $access_token_secret = 'ACCESS_TOKEN_SECRET';
     $connection = new TwitterOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
     $content = $connection->get("account/verify_credentials");
     
